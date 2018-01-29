0b2c8306cc6243ccce674d2f78a6d4fe79dea424
Vadym Shymko <vadymshymko@gmail.com>
2018-01-30 01:07:35 +0200

    Add logo component

---
+++ src/components/AppLogo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AppLogo.scss';
+
+const AppLogo = () => (
+  <span
+    className="AppLogo"
+    title="Goal Magazine"
+  >
+    <strong className="AppLogo__title">Goal Magazine</strong>
+  </span>
+);
+
+export default AppLogo;
