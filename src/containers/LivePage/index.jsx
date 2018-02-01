fe34e4dc983de50d5b98c3bb1ecaf11b82a06a81
Vadym Shymko <vadymshymko@gmail.com>
2018-02-02 01:43:57 +0200

    Connect live page to store

--- src/containers/LivePage/index.jsx
+++ src/containers/LivePage/index.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const LivePage = () => (
-  <div>Live Page!</div>
-);
+import { getFixtures } from 'actions';
 
-export default LivePage;
+import AppPage from 'components/AppPage';
+
+class LivePage extends Component {
+  static propTypes = {
+    fixturesItems: PropTypes.arrayOf(PropTypes.object).isRequired,
+    getFixtures: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    this.props.getFixtures();
+  }
+
+  render() {
+    const {
+      fixturesItems,
+    } = this.props;
+
+    return (
+      <AppPage title="Live Results">
+        {fixturesItems.map((i, index) => (
+          index
+        ))}
+      </AppPage>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  fixturesItems: state.fixtures.items,
+});
+
+const actions = {
+  getFixtures,
+};
+
+export default connect(mapStateToProps, actions)(LivePage);
