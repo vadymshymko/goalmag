ad1684335d8fa29e0105cafd4c9c170bad6c152c
Vadym Shymko <vadymshymko@gmail.com>
2018-02-03 19:56:54 +0200

    Update api for make max 50 requests per minute

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,39 @@
 import config from 'config';
 import { fetch } from 'utils';
 
-export const callApi = (endpoint) => {
+const CALL_API_HEADERS = {
+  'X-Auth-Token': config.apiKey,
+};
+
+let apiRequestsCount = 0;
+let prevApiRequestDate = 0;
+
+export const callApi = endpoint => new Promise((resolve) => {
   const requestUrl = (endpoint.indexOf(config.apiRoot) === -1)
     ? config.apiRoot + endpoint
     : endpoint;
 
-  return fetch(requestUrl, {
-    headers: {
-      'X-Auth-Token': config.apiKey,
-    },
-  });
-};
+  const requestDate = Date.now();
+  const prevRequestDateDiff = requestDate - prevApiRequestDate;
+  const isApiRequestEnabled = (
+    apiRequestsCount < 50
+    || prevRequestDateDiff > 90000
+  );
+
+  if (isApiRequestEnabled) {
+    prevApiRequestDate = requestDate;
+    apiRequestsCount = apiRequestsCount >= 50
+      ? 1
+      : apiRequestsCount + 1;
+
+    resolve(fetch(requestUrl, {
+      headers: CALL_API_HEADERS,
+    }));
+  }
+
+  setTimeout(() => (
+    resolve(callApi(endpoint))
+  ), 90000);
+});
 
 export default callApi;
