28282b18a79406f977dcb1405043f3e0bbf648bd
Vadym Shymko <vadymshymko@gmail.com>
2018-02-03 15:48:36 +0200

    Add teams reducer to main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+
 import competitions from './competitions';
 import fixtures from './fixtures';
+import teams from './teams';
 
 export default combineReducers({
   competitions,
   fixtures,
+  teams,
 });
