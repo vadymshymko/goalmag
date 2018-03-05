ddaf6aa816ec15d45b437212a7648f7ac669e7da
Vadym Shymko <vadymshymko@gmail.com>
2018-03-05 22:23:32 +0200

    Fix app logo color

--- src/components/AppLogo/index.jsx
+++ src/components/AppLogo/index.jsx
@@ -7,7 +7,7 @@ const AppLogo = () => (
     className="AppLogo"
     viewBox="0 0 30 30"
   >
-    <circle cx="15" cy="15" r="15" fill="green" />
+    <circle cx="15" cy="15" r="15" fill="#28a745" />
     <circle cx="15" cy="15" r="3" fill="white" />
     <circle cx="15" cy="15" r="8" fill="none" stroke="white" strokeWidth="2" />
     <line x1="15" y1="0" x2="15" y2="30" stroke="white" strokeWidth="2" />
