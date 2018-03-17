0afcce9822a76af8c790d73d301dd00754a4d359
Vadym Shymko <vadymshymko@gmail.com>
2018-03-18 01:48:06 +0200

    Add webpack config for precompile server config

---
+++ webpack/server.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const NodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './server/server.jsx',
+  ],
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, '../common'),
+      'node_modules',
+    ],
+  },
+  target: 'node',
+  externals: [new NodeExternals()],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new UglifyJSPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              [
+                'babel-plugin-transform-require-ignore',
+                {
+                  extensions: ['.scss', '.css'],
+                },
+              ],
+            ],
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
