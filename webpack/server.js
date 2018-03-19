06aa92b6f8076652885ba1e3e9262893de5d71ba
Vadym Shymko <vadymshymko@gmail.com>
2018-03-19 19:48:27 +0200

    Enable minification && disable define plugin for server webpack config

--- webpack/server.js
+++ webpack/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const webpack = require('webpack');
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const NodeExternals = require('webpack-node-externals');
 
 module.exports = {
@@ -23,12 +23,12 @@ module.exports = {
   target: 'node',
   externals: [new NodeExternals()],
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-//     new UglifyJSPlugin(),
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         NODE_ENV: JSON.stringify('production'),
+//       },
+//     }),
+    new UglifyJSPlugin(),
   ],
   module: {
     rules: [
