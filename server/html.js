991cf67444a4843de121867b363e7534e758c14e
Vadym Shymko <vadymshymko@gmail.com>
2018-03-19 11:19:20 +0200

    Move serve html reply template to separate file

---
+++ server/html.js
@@ -0,0 +1,41 @@
+const renderHTML = ({
+  title = '',
+  meta = '',
+  innerHTML = '',
+  preloadedState = {},
+}) => (`
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      ${title}
+      ${meta}
+
+      <link href="/bundle.css" rel="stylesheet" />
+      <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+      <link rel="manifest" href="/site.webmanifest">
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#28a745">
+      <meta name="msapplication-TileColor" content="#ffffff">
+      <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+      <meta name="theme-color" content="#ffffff">
+    </head>
+    <body>
+      <div class="root" id="root">${innerHTML}</div>
+
+      <script>
+        window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+      </script>
+
+      <script src="/bundle.js"></script>
+    </body>
+  </html>
+`);
+
+export default renderHTML;
