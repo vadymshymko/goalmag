feaaa6a6c2237b0e2afd7663d7dbc150a06b8dd9
Vadym Shymko <vadymshymko@gmail.com>
2018-03-19 19:22:09 +0200

    Delete process env port debug

--- server/server.jsx
+++ server/server.jsx
@@ -86,6 +86,4 @@ app.use(Express.static('dist/assets'));
 app.use(Express.static('static'));
 app.use(handleRequest);
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-});
+app.listen(port);
