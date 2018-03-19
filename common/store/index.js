d77a15f9ef3947454bac816f94ed25dc4e1f7e64
Vadym Shymko <vadymshymko@gmail.com>
2018-03-19 11:18:50 +0200

    Delete preloaded store from window

--- common/store/index.js
+++ common/store/index.js
@@ -3,7 +3,8 @@ import { createStore } from 'redux';
 import rootReducer from 'reducers';
 import composeEnhancers from 'composeEnhancers';
 
-const preloadedState = window.__PRELOADED_STATE__; //eslint-disable-line
+const preloadedState = window.__PRELOADED_STATE__ || {}; //eslint-disable-line
+delete window.__PRELOADED_STATE__; //eslint-disable-line
 
 const configureStore = () => (
   createStore(rootReducer, preloadedState, composeEnhancers())
