d1f365a805b873f28c6daeddd796905380869a85
Vadym Shymko <vadymshymko@gmail.com>
2018-03-20 00:48:47 +0200

    Fix server redirect from home to match-center

--- webpack/server.js
+++ webpack/server.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const NodeExternals = require('webpack-node-externals');
 
@@ -23,11 +22,6 @@ module.exports = {
   target: 'node',
   externals: [new NodeExternals()],
   plugins: [
-//     new webpack.DefinePlugin({
-//       'process.env': {
-//         NODE_ENV: JSON.stringify('production'),
-//       },
-//     }),
     new UglifyJSPlugin(),
   ],
   module: {
