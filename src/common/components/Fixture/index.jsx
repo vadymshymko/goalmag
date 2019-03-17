dd79af26685ea800f22b2df719747d24e8808cee
Vadym Shymko <vadymshymko@gmail.com>
2019-03-17 02:14:19 +0200

    Update styles

--- src/common/components/Fixture/index.jsx
+++ src/common/components/Fixture/index.jsx
@@ -1,10 +1,97 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import FixtureTeam from 'components/FixtureTeam';
 
 import styles from './Fixture.scss';
+
+// SCHEDULED | LIVE | IN_PLAY | PAUSED | FINISHED | POSTPONED | SUSPENDED | CANCELED;
+
+const getScoreStyleByStatus = (status) => {
+  if (status === 'live' || status === 'in_play' || status === 'paused') {
+    return {
+      backgroundColor: '#28a745',
+      color: '#fff',
+    };
+  }
+
+  if (status === 'finished') {
+    return {
+      backgroundColor: '#343a40',
+      color: '#fff',
+    };
+  }
+
+  return {
+    backgroundColor: '#f8f9fa',
+    color: '#343a40',
+  };
+};
+
+const getStatusLabel = (status, startDate) => {
+  if (status === 'finished') {
+    return 'FT';
+  }
+
+  if (status === 'paused') {
+    return 'HT';
+  }
+
+  if (status === 'scheduled') {
+    return 'SCHED';
+  }
+
+  if (status === 'postponed') {
+    return 'POST';
+  }
+
+  if (status === 'suspended') {
+    return 'PEND';
+  }
+
+  if (status === 'canceled') {
+    return 'CAN';
+  }
+
+  if (status === 'live' || status === 'in_play') {
+    const currentDate = moment(Date.now()).local();
+    const diff = currentDate.diff(moment(startDate).local(), 'minutes');
+
+    if (diff <= 0) {
+      return '1′';
+    }
+
+    if (diff < 45) {
+      return `${diff + 1}′`;
+    }
+
+    if (diff < 60) {
+      return '45′';
+    }
+
+    if (diff < 105) {
+      return `${diff - 14}′`;
+    }
+
+    return '90′+';
+  }
+
+  return (status || '').replace(/_/gi, ' ').toUpperCase();
+};
+
+const getStatusStyle = (status) => {
+  if (status === 'live' || status === 'in_play' || status === 'paused' || status === 'finished') {
+    return {
+      color: '#343a40',
+    };
+  }
+
+  return {
+    color: '#6c757d',
+  };
+};
 
 const Fixture = ({
   homeTeamId,
@@ -16,11 +103,11 @@ const Fixture = ({
   awayTeamScore,
   awayTeamLogoUrl,
   status,
-  date,
+  utcDate,
 }) => (
   <article className={styles.Fixture}>
     <span className={styles.Fixture__date}>
-      {date}
+      {moment.utc(utcDate).local().format('HH:mm')}
     </span>
 
     <div className={styles.Fixture__mainInfo}>
@@ -33,9 +120,7 @@ const Fixture = ({
           ? homeTeamScore || 0
           : homeTeamScore || '-'
         }
-        scoreStyle={{
-          backgroundColor: '#f8f9fa',
-        }}
+        scoreStyle={getScoreStyleByStatus(status)}
       />
 
       <span className={styles.Fixture__separator} />
@@ -49,14 +134,12 @@ const Fixture = ({
           ? awayTeamScore || 0
           : awayTeamScore || '-'
         }
-        scoreStyle={{
-          backgroundColor: '#f8f9fa',
-        }}
+        scoreStyle={getScoreStyleByStatus(status)}
       />
     </div>
 
-    <span className={styles.Fixture__status}>
-      {(status || '').replace(/_/gi, ' ')}
+    <span className={styles.Fixture__status} style={getStatusStyle(status)}>
+      {getStatusLabel(status, utcDate)}
     </span>
   </article>
 );
@@ -71,7 +154,7 @@ Fixture.propTypes = {
   awayTeamScore: PropTypes.number,
   awayTeamLogoUrl: PropTypes.string,
   status: PropTypes.string,
-  date: PropTypes.string,
+  utcDate: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 Fixture.defaultProps = {
@@ -84,7 +167,7 @@ Fixture.defaultProps = {
   awayTeamScore: null,
   awayTeamLogoUrl: '',
   status: 'timed',
-  date: '',
+  utcDate: Date.now(),
 };
 
 export default withStyles(styles)(Fixture);
