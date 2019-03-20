e9f90730bb3b4e6a91a16ef4e875c4258495b440
Vadym Shymko <vadymshymko@gmail.com>
2019-03-21 00:34:28 +0200

    Update styles

--- src/common/components/AppContent/index.jsx
+++ src/common/components/AppContent/index.jsx
@@ -9,7 +9,7 @@ import routes from 'routes';
 import LayoutContainer from 'components/LayoutContainer';
 
 const AppContent = () => (
-  <div style={{ maxWidth: '100%' }}>
+  <div style={{ maxWidth: '100%', paddingBottom: '60px' }}>
     <LayoutContainer>
       <Switch>
         {routes.map(({
