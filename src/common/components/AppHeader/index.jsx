65b5f927aad2135373b34cf3cd131bf98551916b
Vadym Shymko <vadymshymko@gmail.com>
2019-03-24 22:04:58 +0200

    Update styles

--- src/common/components/AppHeader/index.jsx
+++ src/common/components/AppHeader/index.jsx
@@ -3,7 +3,6 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import LayoutContainer from 'components/LayoutContainer';
 import AppTitle from 'components/AppTitle';
-// import AppNavContainer from 'containers/AppNavContainer';
 import AppMenu from 'components/AppMenu';
 
 import styles from './AppHeader.scss';
@@ -30,49 +29,33 @@ class AppHeader extends Component {
 
     return (
       <header className={styles.AppHeader}>
-        <div className={styles.AppHeader__banner}>
-          <LayoutContainer>
-            <button
-              className={styles.AppHeader__showNavBtn}
-              type="button"
-              title="Show Navigation"
-              onClick={this.openMenu}
-              aria-label="Show Navigation"
+        <LayoutContainer>
+          <button
+            className={styles.AppHeader__showNavBtn}
+            type="button"
+            title="Show Navigation"
+            onClick={this.openMenu}
+            aria-label="Show Navigation"
+          >
+            <svg
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              stroke="currentColor"
+              className={styles.AppHeader__showNavIcon}
             >
-              <svg
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                stroke="currentColor"
-                className={styles.AppHeader__showNavIcon}
-              >
-                <line x1="3" y1="12" x2="21" y2="12" />
-                <line x1="3" y1="6" x2="21" y2="6" />
-                <line x1="3" y1="18" x2="21" y2="18" />
-              </svg>
-            </button>
+              <line x1="3" y1="12" x2="21" y2="12" />
+              <line x1="3" y1="6" x2="21" y2="6" />
+              <line x1="3" y1="18" x2="21" y2="18" />
+            </svg>
+          </button>
 
-            <AppTitle />
+          <AppTitle />
 
-            <AppMenu isVisible={showMenu} onRequestHide={this.closeMenu} />
-          </LayoutContainer>
-        </div>
-
-        {/* <div className={styles.AppHeader__nav}>
-          <LayoutContainer>
-            <AppNavContainer />
-            <div style={{ background: '#fff', display: 'flex', width: '100%' }}>
-              {Array.from({ length: '10' }).map((item, index) => (
-                <div className="comp-nav-item" key={index}>
-                  <span />
-                  <span>Some Competition</span>
-                </div>
-              ))}
-            </div>
-          </LayoutContainer>
-        </div> */}
+          <AppMenu isVisible={showMenu} onRequestHide={this.closeMenu} />
+        </LayoutContainer>
       </header>
     );
   }
