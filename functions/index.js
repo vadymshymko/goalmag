0d616fc7e8cc1290f0ad420a11b89c2be24629ad
Vadym Shymko <vadymshymko@gmail.com>
2020-03-14 11:54:55 +0200

    Update functions config

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
 const functions = require('firebase-functions');
 const getSSRApp = require('./getSSRApp/').default;
 
-exports.getSSRApp = functions.region('europe-west1').https.onRequest(getSSRApp);
+exports.getSSRApp = functions.https.onRequest(getSSRApp);
